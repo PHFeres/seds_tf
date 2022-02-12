e776206ff8ac969e51af0c5713bfa9d8ca23bcce
PHFeres <pedro.campos@enacom.com.br>
2022-02-12 09:49:35 -0300

    feat ( simulation.py) make simulation of problem

---
+++ simulation.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+lambda_a = 2
+lambda_c = 1000 * lambda_a
+lambda_d1 = 1.0459
+lambda_d2 = 1.8388
+
+
+def eval_va():
+    return np.random.exponential(scale=1 / lambda_a)
+
+
+def eval_vc():
+    return np.random.exponential(scale=1 / lambda_c)
+
+
+def eval_vd1():
+    return np.random.exponential(scale=1 / lambda_d1)
+
+
+def eval_vd2():
+    return np.random.exponential(scale=1 / lambda_d2)
+
+
+X = [0, 0]
+
+sim_size = 10000
+n = 2
+
+def make_simulation():
+    a = np.zeros((sim_size))
+    c = np.zeros((sim_size))
+    d = np.zeros((sim_size))
+
+    for k in range(0, sim_size):
+
+        V_a = eval_va()
+        V_c = eval_vc()
+        V_d1 = eval_vd1()
+        V_d2 = eval_vd2()
+
+        a[k] = max(V_a + a[k-1], c[k-n])
+        c[k] = V_c + max(a[k], d[k-1])
+        d[k] = V_d1 + V_d2 + c[k]
+
+    W_q_each = c[1000:] - a[1000:]
+    # W_q_each[W_q_each > 6] = 2
+    plt.boxplot(W_q_each)
+    W_q = np.mean(W_q_each)
+    print("Tempo médio de espera na fila: ", W_q)
+
+    plt.show()
+
+    return W_q
+
+
+if __name__ == '__main__':
+
+    W_q_list = list()
+    for _ in range(1):
+
+        W_q_list.append(make_simulation())
+
+    plt.boxplot(W_q_list)
+    plt.show()
+    print("Média: ", np.mean(W_q_list))
